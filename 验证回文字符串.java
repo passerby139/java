7716e988917b11779072551939ee76aec56bfad0
passerby139 <passerby18@163.com>
2019-12-12 22:06:27 +0800

    提交代码

---
+++ 验证回文字符串.java
@@ -0,0 +1,31 @@
+package Lee_code;
+
+import java.util.Arrays;
+
+public class 验证回文字符串 {
+	public static void main(String[] args) {
+		System.out.println(validPalindrome("sdfsdfsdfs"));
+	}
+	public static boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return isValid(s, i + 1, j) || isValid(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+    public static boolean isValid(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
